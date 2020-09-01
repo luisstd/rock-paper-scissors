332a26761af7fd065fdcdb6b830490aa716de045
shouhu <mbaazi@mailbox.org>
2020-09-01 18:52:36 +0200

    formatted code

--- script.js
+++ script.js
@@ -9,52 +9,51 @@ const scoreComputerDisplay = document.getElementById("scoreComputer");
 const info = document.getElementById("info");
 
 // button helper function
-let buttonOutput = '';
+let buttonOutput = "";
 
 function getButtonRock() {
-	console.log('rock');
-	buttonOutput = 'rock';
-	playerSelection = 'rock';
-	computerSelection = computerPlay();
+  console.log("rock");
+  buttonOutput = "rock";
+  playerSelection = "rock";
+  computerSelection = computerPlay();
 }
 
 function getButtonPaper() {
-	console.log('paper');
-	buttonOutput = 'paper';
-	playerSelection = 'paper';
-	computerSelection = computerPlay();
-}		
+  console.log("paper");
+  buttonOutput = "paper";
+  playerSelection = "paper";
+  computerSelection = computerPlay();
+}
 
 function getButtonScissors() {
-	console.log('scissors');
-	buttonOutput = 'scissors';
-	playerSelection = 'scissors';
-	computerSelection = computerPlay();
+  console.log("scissors");
+  buttonOutput = "scissors";
+  playerSelection = "scissors";
+  computerSelection = computerPlay();
 }
 
 // plays turn for each opponent
-function playerPlay() {		
-buttonRock.addEventListener('click', getButtonRock);
-buttonPaper.addEventListener('click', getButtonPaper);
-buttonScissors.addEventListener('click', getButtonScissors);	
+function playerPlay() {
+  buttonRock.addEventListener("click", getButtonRock);
+  buttonPaper.addEventListener("click", getButtonPaper);
+  buttonScissors.addEventListener("click", getButtonScissors);
 
-	if (buttonOutput === 'rock') {
-		return 'rock';
-	} else if (buttonOutput === 'paper') {
-		return 'paper' 
-	} else if (buttonOutput === 'scissors') {
-		return 'scissors'
-	}
+  if (buttonOutput === "rock") {
+    return "rock";
+  } else if (buttonOutput === "paper") {
+    return "paper";
+  } else if (buttonOutput === "scissors") {
+    return "scissors";
+  }
 }
-
 
 function computerPlay() {
-	let options = ['rock', 'paper', 'scissors'];
-	return options[Math.floor(Math.random() * Math.floor(3))];
+  let options = ["rock", "paper", "scissors"];
+  return options[Math.floor(Math.random() * Math.floor(3))];
 }
 
-// selections for each opponent	
-let computerSelection = computerPlay(); 
+// selections for each opponent
+let computerSelection = computerPlay();
 let playerSelection = playerPlay();
 
 // score variable
@@ -64,83 +63,82 @@ let scoreComputer = 0;
 // info variable
 
 // game function
-function game(playerSelection, computerSelection) {	
-	playerSelection = playerPlay();
-	computerSelection = computerPlay();
+function game(playerSelection, computerSelection) {
+  playerSelection = playerPlay();
+  computerSelection = computerPlay();
 
-	function playRound(playerSelection, computerSelection) {
-	
-	if(playerSelection === 'rock' && computerSelection === 'paper') {
-	scoreComputer++;
-	scorePlayerDisplay.innerHTML = scorePlayer.toString();
-	scoreComputerDisplay.innerHTML = scoreComputer.toString();
-	info.innerHTML = 'You lose! Paper beats Rock';
-	}
-	else if(playerSelection === 'rock' && computerSelection === 'scissors') {
-	scorePlayer++;
-	scorePlayerDisplay.innerHTML = scorePlayer.toString();
-	scoreComputerDisplay.innerHTML = scoreComputer.toString();
-	info.innerHTML = 'You win! Rock beats Scissors!';
-	}
-	else if(playerSelection === 'rock' && computerSelection === 'rock') {
-	scorePlayerDisplay.innerHTML = scorePlayer.toString();
-	scoreComputerDisplay.innerHTML = scoreComputer.toString();
-	info.innerHTML = 'Tie! Play again';
-	}
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+      scoreComputer++;
+      scorePlayerDisplay.innerHTML = scorePlayer.toString();
+      scoreComputerDisplay.innerHTML = scoreComputer.toString();
+      info.innerHTML = "You lose! Paper beats Rock";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      scorePlayer++;
+      scorePlayerDisplay.innerHTML = scorePlayer.toString();
+      scoreComputerDisplay.innerHTML = scoreComputer.toString();
+      info.innerHTML = "You win! Rock beats Scissors!";
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+      scorePlayerDisplay.innerHTML = scorePlayer.toString();
+      scoreComputerDisplay.innerHTML = scoreComputer.toString();
+      info.innerHTML = "Tie! Play again";
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+      scorePlayerDisplay.innerHTML = scorePlayer.toString();
+      scoreComputerDisplay.innerHTML = scoreComputer.toString();
+      info.innerHTML = "Tie! Play again";
+    } else if (
+      playerSelection === "paper" &&
+      computerSelection === "scissors"
+    ) {
+      scoreComputer++;
+      info.innerHTML = "You lose! Scissors beat Paper!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      scorePlayer++;
+      scorePlayerDisplay.innerHTML = scorePlayer.toString();
+      scoreComputerDisplay.innerHTML = scoreComputer.toString();
+      info.innerHTML = "You win! Paper beats Rock";
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      scorePlayer++;
+      scorePlayerDisplay.innerHTML = scorePlayer.toString();
+      scoreComputerDisplay.innerHTML = scoreComputer.toString();
+      info.innerHTML = "You win! Scissors beat Paper";
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "scissors"
+    ) {
+      scorePlayerDisplay.innerHTML = scorePlayer.toString();
+      scoreComputerDisplay.innerHTML = scoreComputer.toString();
+      info.innerHTML = "Tie! Play again";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      scoreComputer++;
+      scorePlayerDisplay.innerHTML = scorePlayer.toString();
+      scoreComputerDisplay.innerHTML = scoreComputer.toString();
+      info.innerHTML = "You lose! Rock beats Scissors";
+    }
+  }
 
-	else if(playerSelection === 'paper' && computerSelection === 'paper') {
-	scorePlayerDisplay.innerHTML = scorePlayer.toString();
-	scoreComputerDisplay.innerHTML = scoreComputer.toString();
-	info.innerHTML = 'Tie! Play again';
-	}
-	else if(playerSelection === 'paper' && computerSelection === 'scissors') {
-	scoreComputer++;
-	info.innerHTML = 'You lose! Scissors beat Paper!';
-	}
-	else if(playerSelection === 'paper' && computerSelection === 'rock') {
-	scorePlayer++;
-	scorePlayerDisplay.innerHTML = scorePlayer.toString();
-	scoreComputerDisplay.innerHTML = scoreComputer.toString();
-	info.innerHTML = 'You win! Paper beats Rock';
-	}	
-
-	else if(playerSelection === 'scissors' && computerSelection === 'paper') {
-	scorePlayer++;
-	scorePlayerDisplay.innerHTML = scorePlayer.toString();
-	scoreComputerDisplay.innerHTML = scoreComputer.toString();
-	info.innerHTML = 'You win! Scissors beat Paper';
-	}
-	else if(playerSelection === 'scissors' && computerSelection === 'scissors') {
-	scorePlayerDisplay.innerHTML = scorePlayer.toString();
-	scoreComputerDisplay.innerHTML = scoreComputer.toString();
-	info.innerHTML = 'Tie! Play again';
-	}
-	else if(playerSelection === 'scissors' && computerSelection === 'rock') {
-	scoreComputer++;
-	scorePlayerDisplay.innerHTML = scorePlayer.toString();
-	scoreComputerDisplay.innerHTML = scoreComputer.toString();
-	info.innerHTML = 'You lose! Rock beats Scissors';
-	}
-}
-
-	return playRound(playerSelection, computerSelection);
+  return playRound(playerSelection, computerSelection);
 }
 
 function playGame() {
-	game(playerSelection, computerSelection);
-if(scorePlayer === 5 || scoreComputer === 5) {
-	if(scorePlayer > scoreComputer) {
-	info.innerHTML = 'You win! Click another button to restart the game.'; 
-	scorePlayer = 0;
-	scoreComputer = 0;
-	} else if(scorePlayer < scoreComputer) {
-	info.innerHTML = 'Computer wins! Click another button to restart the game.';
-	scorePlayer = 0;
-	scoreComputer = 0;
-	}
-}
+  game(playerSelection, computerSelection);
+  if (scorePlayer === 5 || scoreComputer === 5) {
+    if (scorePlayer > scoreComputer) {
+      info.innerHTML = "You win! Click another button to restart the game.";
+      scorePlayer = 0;
+      scoreComputer = 0;
+    } else if (scorePlayer < scoreComputer) {
+      info.innerHTML =
+        "Computer wins! Click another button to restart the game.";
+      scorePlayer = 0;
+      scoreComputer = 0;
+    }
+  }
 }
 
-buttonRock.addEventListener('click', playGame);
-buttonPaper.addEventListener('click', playGame);
-buttonScissors.addEventListener('click', playGame);
+buttonRock.addEventListener("click", playGame);
+buttonPaper.addEventListener("click", playGame);
+buttonScissors.addEventListener("click", playGame);
